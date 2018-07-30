573e68a214fe1155a057cd02fe42e29c75457d71
Clement MASSARD <cmassard@palo-it.com>
2018-07-30 18:01:37 +0200

    Send messages to current room only

--- server/server.js
+++ server/server.js
@@ -37,19 +37,21 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (newMessage, callback) => {
-    console.log('createMessage', newMessage);
-    var messageTime = moment().format('MMM Do YYYY - h:mm a');
+    const user = users.getUser(socket.id);
 
-    io.emit(
-      'newMessage', 
-      generateMessage(newMessage.from, newMessage.text)
-    );
+    if (user && isRealString(newMessage.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, newMessage.text));
+    }
 
     callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    const user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', (s) => {
